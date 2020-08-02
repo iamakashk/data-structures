4b4215e98aaadc769e8dd11fb62eaf91323fa786
AKASH KAMBLE <kambleakashg@gmail.com>
2020-08-02 23:29:01 +0530

    LinkedList node added .

---
+++ Queue/linkedlist/LinkedListNode.java
@@ -0,0 +1,12 @@
+package com.akash.queue.linkedlist;
+
+public class LinkedListNode {
+
+	private int data;
+	LinkedListNode next;
+	
+	public LinkedListNode( int data) {
+		this.data= data;
+		this.next = null;
+	}
+}
