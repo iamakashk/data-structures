b14479f0c13b18a746c589a014e4a02a6c52095c
AKASH KAMBLE <kambleakashg@gmail.com>
2020-08-03 22:27:47 +0530

    comments added

--- Queue/linkedlist/LinkedListQueue.java
+++ Queue/linkedlist/LinkedListQueue.java
@@ -2,10 +2,32 @@ package com.akash.queue.linkedlist;
 
 public class LinkedListQueue {
 
-	int rear, front, length;
+	int size ;
+	LinkedListNode front , rear ;
 	public LinkedListQueue(){
-		rear = 0;
-		front = 0;
-		length = 0;
+		this.front = this.rear = null;
+		size = 0;
+	}
+	// Inserting into queue
+	public void EnQueue(int data) {
+		LinkedListNode node  = new LinkedListNode(data);
+		if(isEmpty()) {
+			front = node;
+		}else {
+			rear.next = node;
+		}
+	}
+	/**
+	 * @return true if queue is empty
+	 */
+	public boolean isEmpty() {
+		if(size() == 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public int size() {
+		return size;
 	}
 }
